ce55848a6b3e8bf3c89e04a05ba0cd29286b250f
frog1014 <garyfrog2001@hotmail.com>
2019-09-14 04:25:29 +0800

    refactor

--- src/assets/js/common.js
+++ src/assets/js/common.js
@@ -4,6 +4,7 @@ export const NEW_TAB_URL = 'chrome://newtab/'
 export const CONTENTEDITABLE_URL = 'data:text/html, <html contenteditable>'
 export const EXTENSIONS_URL = 'chrome://extensions/shortcuts'
 export const TAB_ID_NONE = chrome.tabs.TAB_ID_NONE
+export const GOOGLE_SEARCH_URL = 'https://www.google.com/search?q='
 export class Common {
 
     static getPasted() {
