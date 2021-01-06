8f1ff03c22845c713d14972e9d697535fd82a97a
Gary <gary@innotech.me>
2021-01-06 13:26:25 +0800

    ready to release

--- README.md
+++ README.md
@@ -1,31 +1,18 @@
-### Lazy Chrome
+### Lazy Mobile01
 
-Lazy Chrome is a mouse terminator
+Lazy Mobile01 is a mouse terminator
 A keyboard shortcuts customization tool of supported features
 
-Super speed up your common operations with your custom shortcut keys
-duplicate, pin or paste URL and open in a new tab!
+Super optimize your user experience with your custom shortcut keys on Mobile01.
+Whether you're into any pages that with pagination, there is a fluent surffing for you.
 *********
-[Lazy Chrome - Chrome 線上應用程式商店](https://chrome.google.com/webstore/detail/lazy-chrome/boijmdgjfcoancndoiclnglhjakeopch "Lazy Chrome - Chrome 線上應用程式商店")
-
 ## Features
 click the app icon to config your shortcuts
 the default actions:
 
-- pin: alt + 1
-- duplicate: alt + 3
-- open copied text: ctrl + shift + v
-- search for copied text: ctrl + shift + v
-- search for selected text
-- move a tab to a new window
-- open notepad in Chrome
-- mute current tab
-- mute other tabs
-- only keep the same domain tabs
-- close the same domain tabs
-- close tabs from the same domain other than current tab
-- copy the URL of the current tab
-- prevent close tab (for preventing the window from closing without user's confirm)
-- go back to the previous tab(toggle 2 most recently used tabs (MRU))
-- simplify title of a bookmark created by dragging
+- go to the next page
+- go to the previous page
+- go to the first page
+- go to the last page
+- go back to the topic list page
 <!-- - go back to the previous tab of last window -->
--- src/entry/popup.js
+++ src/entry/popup.js
@@ -3,14 +3,15 @@ import {
     IS_BOOKMARK_TITLE_SIMPLIFIER_TAG,
     EXTENSIONS_URL,
 } from "../assets/js/common"
+import manifest from '../manifest.json'
 import '../assets/css/popup.css'
 import Api from "../assets/js/api"
 
 (function () {
-
     document.querySelector('#popup_splash_1').innerHTML = Api.getI18nMsg("appDesc")
+    document.querySelector('.splash-head').innerHTML = manifest.name
     document.querySelector('#go_shortcut_setting').applyy(_ => {
-        _.innerHTML = '<i class="fa fa-cog"></i> ' + Api.getI18nMsg("go_shortcut_setting")
+        _.innerHTML = '<i class="fa fa-cog"></i> ' + Api.getI18nMsg("go_shortcut_setting", [manifest.name])
         _.addEventListener('click', event => {
             chrome.tabs.create({
                 url: EXTENSIONS_URL
@@ -18,56 +19,17 @@ import Api from "../assets/js/api"
         })
     })
 
-    document.querySelector('#check-prevent-close-tab').let(it => {
-        Api.getStorageData(IS_PREVENT_CLOSE_TAB_TAG, value => {
-            it.checked = value
-        })
-
-        it.addEventListener('change', e => {
-            Api.setStorageData({
-                [IS_PREVENT_CLOSE_TAB_TAG]: e.target.checked
-            }, _ => {
-                console.log('ok')
-                e.target.checked && Api.getTabs(tabs => {
-                    var targetUrl = Api.getPreventCloseTabUrl();
-                    !tabs.find(tab => tab.url == targetUrl) && chrome.tabs.create({
-                        url: targetUrl,
-                        pinned: true
-                    })
+    chrome.commands.getAll(function (res) {
+        console.log('getAll', res)
+        res && res.length > 0 && res.let(it => {
+            ('<ul>' + it.map(element => '<li>' + element.description + ': ' + element.shortcut + '</li>').join('') + '</ul>')
+            .let(it => Api.getI18nMsg("feature_head") + it
+                    //  + Api.getI18nMsg("features2", ['<br/>'])
+                )
+                .let(it => {
+                    document.querySelector('#features').innerHTML = it
                 })
-            })
-        })
+        });
     })
-
-    document.querySelector('#check-bookmark-title-simplifier').let(it => {
-        Api.getStorageData(IS_BOOKMARK_TITLE_SIMPLIFIER_TAG, value => {
-            it.checked = value
-        })
-
-        it.addEventListener('change', e => {
-            Api.setStorageData({
-                [IS_BOOKMARK_TITLE_SIMPLIFIER_TAG]: e.target.checked
-            }, _ => {
-                console.log('ok')
-            })
-        })
-    })
-
-    document.querySelector('#popup_splash_prevent_close_tab').applyy(_ => {
-        _.innerHTML = Api.getI18nMsg("popup_splash_prevent_close_tab")
-        _.setAttribute('data-tip', Api.getI18nMsg("popup_splash_prevent_close_tab_tooltip"))
-    })
-
-    document.querySelector('#bookmark_title_simplifier_tooltip').applyy(_ => {
-        _.innerHTML = Api.getI18nMsg("bookmark_title_simplifier")
-        _.setAttribute('data-tip', Api.getI18nMsg("bookmark_title_simplifier_tooltip"))
-    })
-
-    Api.getI18nMsg("features")
-        .let(it => '<ul>' + it.split(',').map(element => '<li>' + element.trim() + '</li>').join('') + '</ul>')
-        .let(it => Api.getI18nMsg("feature_head") + it + Api.getI18nMsg("features2", ['<br/>']))
-        .let(it => {
-            document.querySelector('#features').innerHTML = it
-        })
 
 })(window)
